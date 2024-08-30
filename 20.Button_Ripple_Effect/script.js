427c9057a0ce1b78d4606eaf052291f88a9726ef
Jordan <rhezajordan@gmail.com>
2024-08-29 22:16:03 -0500

    Twenti eth

---
+++ 20.Button_Ripple_Effect/script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('.ripple')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.pageX
+        const y = e.pageY
+
+        const buttonLeft = e.target.offsetLeft
+        const buttonTop = e.target.offsetTop
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        button.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 5000)
+    })
+})
+
